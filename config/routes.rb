efbabea089eaa054c8d7ffa13725b4eeae1a6ed9
reina1117 <122454145+reina1117@users.noreply.github.com>
2023-03-06 14:39:20 +0000

    [Finish] 4-19

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
   get "/homes/about" => "homes#about", as: "about"
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+   resource :favorites, only: [:create, :destroy]
    resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
